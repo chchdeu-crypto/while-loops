ec0f632af41351d03b277c3a6da3140c05f8ad4a
chaim <chchdeu@gmail.com>
2026-06-24 15:23:45 +0300

    mission_4

--- loops.py
+++ loops.py
@@ -6,8 +6,27 @@
 #     a+=1
 
 #mission 2
-numbers_2=[10,9,8,7,6,5,4,3,2,1]
+# numbers_2=[10,9,8,7,6,5,4,3,2,1]
+# i=0
+# while i < len(numbers_2):
+#     print(numbers_2[i])
+#     i+=1
+# #mission 3
+# total=0
+# num=0
+# while num<11:
+#     total += num
+#     num+=1
+#     print(total)
+#mission 4
+items=[2,4,6,8]
 i=0
-while i < len(numbers_2):
-    print(numbers_2[i])
+while i < len(items):
+    print(items[i])
     i+=1
+    if items[i]>5:
+        break
+
+
+      
+
